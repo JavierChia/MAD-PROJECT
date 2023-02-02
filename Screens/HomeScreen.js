b14005dadadd8e2a2214279356fed8d203f771f9
JavierChia <javierchia.22@ichat.sp.edu.sg>
2023-02-02 14:58:06 +0800

    final: i hope

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -28,7 +28,6 @@ import themeContext from "../config/themeContext";
 import theme from "../config/theme";
 
 const auth = getAuth(firebase);
-const user = auth.currentUser;
 const db = getFirestore(firebase);
 
 export default function NewHomeScreen() {
@@ -72,11 +71,11 @@ export default function NewHomeScreen() {
     }
   };
 
-  const Task = ({ title, done, id }) => {
+  const Task = ({ title, done, id, deadline }) => {
     const [isChecked, setChecked] = useState(done);
     return (
       <View
-        style={[styles.tasks, { backgroundColor: theme.cardBackgroundColor }]}
+        style={[styles.tasks, { borderWidth: 2, borderColor: theme.color, backgroundColor: done ? '#36da45' : (deadline.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)) ? '#fd3259' : theme.cardBackgroundColor }]}
       >
         <Text style={[styles.tasksTitle, { color: theme.color }]}>{title}</Text>
         <CheckBox
@@ -91,134 +90,58 @@ export default function NewHomeScreen() {
     );
   };
 
+  const sections = [
+    {
+      title: "Today's Tasks",
+      data: tasksData.filter(
+        (task) =>
+          !task.done &&
+          task.deadline.toDateString() === new Date().toDateString()
+      ),
+    },
+    {
+      title: "Overdue",
+      data: tasksData.filter(
+        (task) =>
+          !task.done &&
+          task.deadline.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
+      ),
+    },
+    {
+      title: "Completed",
+      data: tasksData.filter((task) => task.done && task.deadline.toDateString() === new Date().toDateString()),
+    },
+  ];
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: theme.backgroundColor }}>
       <View style={styles.headerContainer}>
         <Image
           style={{ width: "40%", height: "54%" }}
           source={require("./logoH.png")}
-        ></Image>
+        />
       </View>
 
-      {tasksData.filter(
-        (task) =>
-          !task.done &&
-          task.deadline.toDateString() === new Date().toDateString()
-      ).length > 0 && (
-        <View style={styles.header}>
-          <Text style={[styles.headerText, { color: theme.color }]}>
-            Today's Tasks
-          </Text>
-        </View>
-      )}
-      {tasksData
-        .filter(
-          (task) =>
-            !task.done &&
-            task.deadline.toDateString() === new Date().toDateString()
-        )
-        .map((task) => (
-          <View>
-            <Task title={task.name} done={task.done} id={task.id} />
+      <SectionList
+        sections={sections.filter((section) => section.data.length > 0)}
+        stickySectionHeadersEnabled={false}
+        renderSectionHeader={({ section: { title } }) => (
+          <View style={styles.header}>
+            <Text style={[styles.headerText, { color: title === "Overdue" ? '#fd3259' : (title === "Completed" ? '#36da45' : theme.color) }]}>
+              {title}
+            </Text>
           </View>
-        ))}
-
-      {tasksData.filter(
-        (task) =>
-          !task.done &&
-          task.deadline.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
-      ).length > 0 && (
-        <View style={styles.header}>
-          <Text style={[styles.headerText, { color: theme.color }]}>
-            Overdue
-          </Text>
-        </View>
-      )}
-      {tasksData
-        .filter(
-          (task) =>
-            !task.done &&
-            task.deadline.setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
-        )
-        .map((task) => (
+        )}
+        renderItem={({ item }) => (
           <View>
-            <Task title={task.name} done={task.done} id={task.id} />
+            <Task title={item.name} done={item.done} id={item.id} deadline={item.deadline} />
           </View>
-        ))}
-
-      {tasksData.filter((task) => task.done).length > 0 && (
-        <View style={styles.header}>
-          <Text style={[styles.headerText, { color: theme.color }]}>
-            Completed
-          </Text>
-        </View>
-      )}
-      {tasksData
-        .filter((task) => task.done)
-        .map((task) => (
-          <View>
-            <Task title={task.name} done={task.done} id={task.id} />
-          </View>
-        ))}
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </SafeAreaView>
   );
 }
-
-{
-  /* <SectionList
-                style={[styles.sectionList, { backgroundColor: theme.backgroundColor }]}
-                stickySectionHeadersEnabled={false}
-                sections={[
-                    { title: 'Today\'s Tasks', data: taskData.filter(item => !item.deadline == new Date()) },
-                    { title: 'Overdue', data: taskData.filter(item => item.Overdue) }, //compare deadline to current dead, if its less/more == overdue
-                    { title: 'Completed', data: taskData.filter(item => item.done === true) },
-                ]}
-                renderItem={({ item, index }) => (
-                    <Pressable key={index}>
-                      
-                        
-                    </Pressable>
-                )}
-                renderSectionHeader={({ section: { title } }) => (
-
-                    <Text style={[styles.sectionHeader, { color: theme.color }]}>{title}</Text>
-                )}
-                keyExtractor={(item, index) => item + index}
-            /> */
-}
-
-// const tasksAndListNames = [];
-// for (const list of listsData) {
-//   const tasksRef = query(collection(db, "users", uid, "Lists", list.id, "Tasks"));
-//   const tasks = await getDocs(tasksRef);
-//   const taskData = tasks.map((task) => {
-//     return { taskId: task.id, ...task.data() };
-//   });
-
-//   for (const task of taskData) {
-//     tasksAndListNames.push({
-//       listId: list.id,
-//       listName: list.name,
-//       taskId: task.taskId,
-//       task: task.data(),
-//     });
-//   }
-// }
-
-// return tasksAndListNames;
-
-// def get_tasks_and_list_names(user_id):
-//   tasks_and_list_names = []
-//   lists_ref = db.collection(u'users').document(user_id).collection(u'lists')
-//   lists = lists_ref.stream()
-//   for list_doc in lists:
-//       list_name = list_doc.to_dict()['name']
-//       tasks_ref = list_doc.reference.collection(u'tasks')
-//       tasks = tasks_ref.stream()
-//       for task_doc in tasks:
-//           task = task_doc.to_dict()
-//           tasks_and_list_names.append((list_name, task))
-//   return tasks_and_list_names
 
 const styles = StyleSheet.create({
   tasks: {
@@ -229,6 +152,7 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center"
   },
   taskRow: {
     color: "#000",
@@ -241,7 +165,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   tasksTitle: {
-    fontSize: 20,
+    fontSize: 24,
+    fontWeight: '600'
   },
 
   headerContainer: {
@@ -255,52 +180,3 @@ const styles = StyleSheet.create({
     tintColor: "pink",
   },
 });
-
-//useEffect(() => {
-//   onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//       setUid(user.uid);
-//       ListName(user.uid);
-//       taskData(user.uid, allLists);
-//       test();
-//     }
-//   });
-
-// }, []);
-
-// const ListName = async (uid) => {
-//   const listsRef = collection(db, "users", uid, "Lists");
-//   onSnapshot(listsRef, (lists) => {
-//     const listsIDs = lists.docs
-//       .map((listData) => ({
-//         id: listData.id,
-//       }))
-//     setAllLists(listsIDs)
-//   })
-// }
-
-// const querySnapshot =  getDocs(collection(db, "cities"));
-// querySnapshot.forEach((doc) => {
-//   // doc.data() is never undefined for query doc snapshots
-//   console.log(doc.id, " => ", doc.data());
-// });
-
-// const taskData = async (uid, lists) => {
-//   let allTasksData = [];
-//   for (let i = 0; i < lists.length; i++) {
-//     const tasksRef = collection(db, "users", uid, "Lists", lists[i].id, "Tasks")
-//     onSnapshot(tasksRef, (tasks) => {
-//       const tasksData = tasks.docs
-//         .map((allTaskData) => ({
-//           listsID: lists[i].id,
-//           id: allTaskData.id,
-//           deadline: allTaskData.data().deadline.nanoseconds,
-//           desc: allTaskData.data().desc,
-//           done: allTaskData.data().done,
-//           name: allTaskData.data().name,
-//         }))
-//       allTasksData = allTasksData.concat(tasksData);
-//     })
-//   }
-//   setAllTasksData(allTasksData);
-// }
--- Screens/SettingsScreen.js
+++ Screens/SettingsScreen.js
@@ -1,174 +1,410 @@
-import React, { Fragment, useState, useEffect, useContext } from 'react';
-import { SafeAreaView, View, Text, StyleSheet, SectionList, TouchableOpacity, Switch } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React, { Fragment, useState, useEffect, useContext } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  SectionList,
+  TouchableOpacity,
+  Switch,
+  TextInput,
+  Pressable,
+  Linking,
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
-import firebase from './firebase';
-import { doc, updateDoc, getFirestore, getDoc } from 'firebase/firestore'
-import { getAuth, signOut, onAuthStateChanged } from "firebase/auth"
+import firebase from "./firebase";
+import {
+  doc,
+  updateDoc,
+  getFirestore,
+  getDoc,
+} from "firebase/firestore";
+import {
+  getAuth,
+  signOut,
+  onAuthStateChanged,
+  updatePassword,
+  deleteUser,
+} from "firebase/auth";
 
-import {EventRegister} from 'react-native-event-listeners';
-import themeContext from '../config/themeContext';
+import { EventRegister } from "react-native-event-listeners";
+import themeContext from "../config/themeContext";
+import Modal from "react-native-modal";
 
 const auth = getAuth(firebase);
 const user = auth.currentUser;
 const db = getFirestore(firebase);
 
 const SETTINGS = [
-    {
-        title: 'PREFERENCES',
-        data: [
-            { id: 'language', icon: 'globe-outline', label: 'Language', status: 'English', icon2: 'chevron-forward-outline' },
-            { id: 'mode', icon: 'contrast', label: 'Dark Mode', status: 'OFF', icon2: 'chevron-forward-outline' },
-        ]
-    },
-    {
-        title: 'HELP',
-        data: [
-            { id: 'bug', icon: 'flag-outline', label: 'Report Bug', icon2: 'chevron-forward-outline' },
-            { id: 'contact', icon: 'mail-outline', label: 'Contact Us', icon2: 'chevron-forward-outline' },
-        ]
-    },
-    {
-        title: 'ACCOUNT SETTINGS',
-        data: [
-            { id: 'resetpw', icon: 'finger-print-outline', label: 'Reset Password', icon2: 'chevron-forward-outline' },
-            { id: 'logout', icon: 'log-out-outline', label: 'Log Out', icon2: '' },
-        ]
-    }
-]
+  {
+    title: "PREFERENCES",
+    data: [
+      {
+        id: "mode",
+        icon: "contrast",
+        label: "Dark Mode",
+        status: "OFF",
+        icon2: "chevron-forward-outline",
+      },
+    ],
+  },
+  {
+    title: "HELP",
+    data: [
+      {
+        id: "contact",
+        icon: "mail-outline",
+        label: "Contact Us",
+        icon2: "chevron-forward-outline",
+      },
+    ],
+  },
+  {
+    title: "ACCOUNT SETTINGS",
+    data: [
+      {
+        id: "resetpw",
+        icon: "finger-print-outline",
+        label: "Reset Password",
+        icon2: "chevron-forward-outline",
+      },
+      {
+        id: "deleteAcc",
+        icon: "ios-remove-circle-outline",
+        label: "Delete Account",
+        icon2: "",
+      },
+      { id: "logout", icon: "log-out-outline", label: "Log Out", icon2: "" },
+    ],
+  },
+];
 
 export default function SettingsScreen({ navigation }) {
-    const [darkMode, setDarkMode] = useState(false);
-    const [uid, setUid] = useState('');
-    const theme = useContext(themeContext);
+  const [darkMode, setDarkMode] = useState(false);
+  const [uid, setUid] = useState("");
+  const theme = useContext(themeContext);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [password, setPassword] = useState("");
+  const [cPassword, setcPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showCPassword, setShowCPassword] = useState(false);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUid(user.uid);
-                getMode(user.uid)
-            }
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUid(user.uid);
+        getMode(user.uid);
+      }
+    });
+  }, []);
+
+  const onPressHandler = (id) => {
+    if (id === "logout") {
+      signOut(auth)
+        .then(() => {})
+        .catch((error) => {
+          // An error happened.
         });
-    }, []);
-
-    const onPressHandler = (id) => {
-        if (id === 'logout') {
-            signOut(auth).then(() => {
-
-            }).catch((error) => {
-                // An error happened.
-            });
-        }
     }
-
-    const getMode = async (uid) => {
-        const docRef = doc(db, "users", uid);
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-            setDarkMode(docSnap.data().mode);
-        } else {
-
-        }
+    if (id === "resetpw") {
+      setIsModalVisible(true);
     }
-
-    const updateMode = (value) => {
-        try {
-            const ref = doc(db, "users", uid);
-            updateDoc(ref, {
-                mode: value,
-            })
-        } catch (error) {
-            alert('Error toggling' + error);
-        }
+    if (id === "contact") {
+      Linking.openURL("mailto:support@done.com");
     }
+    if (id === "deleteAcc") {
+    const user = auth.currentUser;
+      deleteUser(user)
+        .then(() => {
+          alert("Deleted Acc!");
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    }
+  };
 
-    return (
-        <SafeAreaView style={[styles.background, {backgroundColor: theme.backgroundColor}]}>
-            <View style={styles.headerContainer}>
-                <Text style={styles.header}>Settings</Text>
+  const getMode = async (uid) => {
+    const docRef = doc(db, "users", uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      setDarkMode(docSnap.data().mode);
+    } else {
+    }
+  };
+
+  const updateMode = (value) => {
+    try {
+      const ref = doc(db, "users", uid);
+      updateDoc(ref, {
+        mode: value,
+      });
+    } catch (error) {}
+  };
+
+  const auth = getAuth();
+
+  const onResetPW = (pw) => {
+    const user = auth.currentUser;
+    const newPassword = pw;
+    if (password !== "" && cPassword !== "") {
+      if (password === cPassword) {
+        updatePassword(user, newPassword)
+          .then(() => {
+            setIsModalVisible(!isModalVisible);
+            alert("Password has been reset!");
+          })
+          .catch((error) => {
+            switch (error.code) {
+              case "auth/weak-password":
+                alert("Password should be at least 6 characters!");
+                break;
+              default:
+                alert(error.message);
+            }
+          });
+      } else {
+        alert("Passwords do not match!");
+      }
+    } else {
+      alert("Please fill up all the fields!");
+    }
+  };
+
+  return (
+    <SafeAreaView
+      style={[styles.background, { backgroundColor: theme.backgroundColor }]}
+    >
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setIsModalVisible(!isModalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: "bold",
+                marginTop: 15,
+                marginBottom: 10,
+              }}
+            >
+              Reset Password
+            </Text>
+            <TextInput
+              style={styles.pwTextInput}
+              placeholder="New Password"
+              textContentType="password"
+              secureTextEntry={!showPassword}
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            ></TextInput>
+
+            <TextInput
+              style={styles.pwTextInput}
+              placeholder="Confirm New Password"
+              textContentType="password"
+              secureTextEntry={!showCPassword}
+              value={cPassword}
+              onChangeText={(text) => setcPassword(text)}
+            ></TextInput>
+            <View
+              style={{
+                width: "65%",
+                flexDirection: "row",
+                justifyContent: "space-around",
+              }}
+            >
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => onResetPW(password)}
+              >
+                <Text style={styles.textStyle}>Reset Password!</Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button2, styles.buttonClose2]}
+                onPress={() => setIsModalVisible(!isModalVisible)}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </Pressable>
             </View>
-            <SectionList
-                sections={SETTINGS}
-                keyExtractor={(item, index) => item + index}
-                renderItem={({ item }) =>
-                    <TouchableOpacity onPress={() => onPressHandler(item.id)} style={[styles.settingOptions, {backgroundColor: theme.cardBackgroundColor}]}>
-                        <Icon name={item.icon} size={26} style={[styles.iconStyle, {color: theme.color}]} />
-                        <Text style={[styles.labelStyle, {color: theme.color}]}>{item.label}</Text>
-                        {item.id === 'mode' ? (
-                            <Switch
-                                value={darkMode}
-                                onValueChange={(value) => { 
-                                    setDarkMode(value), 
-                                    updateMode(value) 
-                                    EventRegister.emit("changeTheme", darkMode)
-                                }}
-                            />
-                        ) : (
-                            <>
-                                <Text style={[styles.statusStyle, {color: theme.color}]}>{item.status}</Text>
-                                <Icon name={item.icon2} size={26} style={[styles.icon2Style, {color: theme.color}]} />
-                            </>
-                        )}
-                    </TouchableOpacity>
-                }
-                renderSectionHeader={({ section: { title } }) => (
-                    <Text style={styles.settingsSectionHeader}>{title}</Text>
-                )}
-            />
+          </View>
+        </View>
+      </Modal>
 
-        </SafeAreaView>
-    )
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Settings</Text>
+      </View>
+      <SectionList
+        sections={SETTINGS}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => onPressHandler(item.id)}
+            style={[
+              styles.settingOptions,
+              { backgroundColor: theme.cardBackgroundColor },
+            ]}
+          >
+            <Icon
+              name={item.icon}
+              size={26}
+              style={[styles.iconStyle, { color: theme.color }]}
+            />
+            <Text style={[styles.labelStyle, { color: theme.color }]}>
+              {item.label}
+            </Text>
+            {item.id === "mode" ? (
+              <Switch
+                value={darkMode}
+                onValueChange={(value) => {
+                  setDarkMode(value), updateMode(value);
+                  EventRegister.emit("changeTheme", darkMode);
+                }}
+              />
+            ) : (
+              <>
+                <Text style={[styles.statusStyle, { color: theme.color }]}>
+                  {item.status}
+                </Text>
+                <Icon
+                  name={item.icon2}
+                  size={26}
+                  style={[styles.icon2Style, { color: theme.color }]}
+                />
+              </>
+            )}
+          </TouchableOpacity>
+        )}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.settingsSectionHeader}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1,
+  background: {
+    flex: 1,
+  },
+  headerContainer: {
+    height: "11%",
+    backgroundColor: "#000",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#fff",
+    margin: 20,
+  },
+  settingsSectionHeader: {
+    marginHorizontal: 20,
+    marginTop: 30,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#a7a7a7",
+  },
+  settingOptions: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 15,
+    marginTop: 10,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  iconStyle: {
+    marginRight: 15,
+  },
+  icon2Style: {
+    marginLeft: 3,
+    // color: '#616161'
+  },
+  labelStyle: {
+    flex: 1,
+    fontSize: 22,
+    fontWeight: "400",
+  },
+  statusStyle: {
+    fontSize: 20,
+    color: "#616161",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    width: "90%",
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    headerContainer: {
-        height: '11%',
-        backgroundColor: '#000',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    header: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: '#fff',
-        margin: 20
-    },
-    settingsSectionHeader: {
-        marginHorizontal: 20,
-        marginTop: 30,
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#a7a7a7'
-    },
-    settingOptions: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        padding: 15,
-        marginTop: 10,
-        marginHorizontal: 20,
-        borderRadius: 10,
-        overflow: 'hidden',
-    },
-    iconStyle: {
-        marginRight: 15,
-    },
-    icon2Style: {
-        marginLeft: 3,
-        // color: '#616161'
-    },
-    labelStyle: {
-        flex: 1,
-        fontSize: 22,
-        fontWeight: '400',
-    },
-    statusStyle: {
-        fontSize: 20,
-        color: '#616161',
-    }
-})
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  pwTextInput: {
+    width: "90%",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginVertical: 5,
+    fontSize: 18,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#333",
+  },
+  button2: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    marginTop: 10,
+    marginBottom: 15,
+  },
+  buttonOpen2: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose2: {
+    backgroundColor: "#ff0000",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});

b18a3893b762ad5ae72f8cb69023f6caa3fe5ffb
JavierChia <javierchia.22@ichat.sp.edu.sg>
2023-02-02 16:43:05 +0800

    updated

---
+++ config/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+    light: {
+        theme: "light",
+        color: 'black',
+        border2Color: '#000',
+        backgroundColor: '#e8e8e8',
+        cardBackgroundColor: '#fff',
+        borderColor: '#000',
+        c1: 'black',
+        c2: 'black'
+    },
+    dark: {
+        theme: "dark",
+        color: 'white',
+        border2Color: '#333',
+        backgroundColor: '#141414',
+        cardBackgroundColor: '#282828',
+        borderColor: '#fff',
+        c1: 'black',
+        c2: 'white'
+    }
+}
+export default theme;
